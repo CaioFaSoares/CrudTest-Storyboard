735d17df4ecb31442439282132cb5c85ca6aaa33
Caio Soares <caiofasoares@gmail.com>
2022-10-03 13:47:15 -0300

    Configured the Add Student View Controller

--- crudTest/AddStudentVC.swift
+++ crudTest/AddStudentVC.swift
@@ -5,4 +5,43 @@
 //  Created by Caio Soares on 03/10/22.
 //
 
-import Foundation
+import UIKit
+
+class AddStudentVC: UIViewController {
+    
+    
+
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtStd: UITextField!
+    @IBOutlet weak var txtSchool: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func AddOnClick(_ sender: Any) {
+        if let name = txtName.text,
+           let std = txtStd.text,
+           let school = txtSchool.text {
+            
+            let newStudent = Student(context: DBManager.share.context)
+            newStudent.name     = name
+            newStudent.std      = std
+            newStudent.school   = school
+            
+            DBManager.share.saveContext()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
